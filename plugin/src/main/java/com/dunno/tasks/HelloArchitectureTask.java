989ca153826b16558e0e6d31080ab9ff56957372
Joan Ferley Mosquera Lozano <mosqueralozanojhoanferley@gmail.com>
2026-08-07 12:20:25 -0500

    feat: add functionality to get tasks' properties from the cli
    - the idea configuration changed

--- plugin/src/main/java/com/dunno/tasks/HelloArchitectureTask.java
+++ plugin/src/main/java/com/dunno/tasks/HelloArchitectureTask.java
@@ -4,8 +4,13 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.options.Option;
 
 public abstract class HelloArchitectureTask extends DefaultTask {
+
+    @Input
+    @Option(option = "developer-name", description = "The developer name to greet")
+    public abstract Property<String> getDeveloperName();
 
     @Input
     public abstract Property<String> getPack();
@@ -19,6 +24,8 @@ public abstract class HelloArchitectureTask extends DefaultTask {
     @TaskAction
     void run() {
         System.out.println("Hello from Spring Architecture plugin!");
+
+        System.out.println("Hope you doing well, " + getDeveloperName().get());
 
         System.out.printf(
                 "Package name: %s\nProject name: %s\nJava version: %s\n",
